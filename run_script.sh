3240dc9639e35052767efaed7b80bb3c7a6d7f13
Jonathan Simmonds <js9721@bris.ac.uk>
2012-12-13 01:37:47 +0000

    added comments to shell scripts to clarify usage

--- run_script.sh
+++ run_script.sh
@@ -1,6 +1,13 @@
+#!/bin/sh
+
+# AUTHOR:      Jonathan Simmonds
+# DATE:        12/12/12
+# DESCRIPTION: THIS FILE SHOULD LIVE ON THE REMOTE (PANDABOARD).
+#              First compiles (if necessary) before running and bringing any output files back.
+
 make
 cd ../Bin/Arm-Release/
 ./Sample-NiSimpleRead
 cd -
 mv ../Bin/Arm-Release/*.dat .
-mv ../Bin/Arm-Release/*.csv .
+#mv ../Bin/Arm-Release/*.csv .
--- sync.sh
+++ sync.sh
@@ -2,7 +2,8 @@
 
 # AUTHOR:      Jonathan Simmonds
 # DATE:        12/12/12
-# DESCRIPTION: Syncs data from the jpanda's working directory to the desktop's git repo.
+# DESCRIPTION: THIS FILE SHOULD LIVE ON THE HOST (DESKTOP) MACHINE.
+#              Syncs data from the jpanda's working directory to the desktop's git repo.
 
 echo "Syncing with jpanda (pulling changes)."
 rsync -vlptgu -e "ssh" jpanda:~/kinect/OpenNI/Platform/Linux/Redist/OpenNI-Bin-Dev-Linux-Arm-v1.5.4.0/Samples/NiSimpleRead/* .
